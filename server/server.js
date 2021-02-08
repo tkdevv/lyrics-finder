a7cd87c008368cb74933b0a4829a59095ac803a4
2skee <anbuwoods@gmail.com>
2021-02-08 17:35:31 +0200

    added sersver for heroku deployment

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, "..", "build");
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.listen(port, () => {});
